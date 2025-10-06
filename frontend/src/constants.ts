4647cc1586cb33a6794ffd37a2ba735c769b8234
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-10-05 21:58:20 -0400

    constants

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -9,8 +9,14 @@ const inferSecureFallback = () => {
 };
 
 export const API_BASE_URLS = [
-  import.meta.env.VITE_API_PRIMARY ?? 'http://localhost:8000',
-  import.meta.env.VITE_API_SECONDARY ?? 'http://159.203.5.107:8000',
+  import.meta.env.VITE_API_PRIMARY,
+  import.meta.env.VITE_API_SECONDARY,
+  'http://localhost:8000',
+  'http://127.0.0.1:8000',
+  'http://159.203.5.107:8000',
+  'http://159.203.5.107',
+  'https://159.203.5.107:8000',
+  'https://159.203.5.107',
   inferSecureFallback(),
 ] as const;
 
